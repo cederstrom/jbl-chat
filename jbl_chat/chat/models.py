f79b322d14e14f37f9258d79b710c8bf1666946f
Andreas Cederström <andreas.cederstrom@factor10.com>
2022-09-04 22:46:22 +0200

    Added support for users and auth

--- jbl_chat/chat/models.py
+++ jbl_chat/chat/models.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# user
-  # id: guid
-  # handle: string
+
+class User(AbstractUser):
+    pass
 
 # conversation
   # id
--- jbl_chat/chat/views.py
+++ jbl_chat/chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+from chat.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
